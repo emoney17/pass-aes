dfe82a5a7f900958b54def2d69e09fafb0a92d83
emoney17 <emmanuel.million@tutanota.com>
2023-01-17 00:07:35 -0600

    encryption progress

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,12 @@
 #include "hex.h"
 
 #include <iostream>
-#include <fstream>
 #include <ostream>
 #include <string>
+
+#include <vector>
+#include <cstdint>
+#include <iomanip>
 
 class User
 {
@@ -33,117 +36,241 @@ class User
             std::cout << "Data File: " << PassFile << std::endl;
             std::cout << "Key File: " << KeyFile << std::endl;
         }
+
 };
 
-// Function to generate a key along with iv
-void genKey()
+void generatePassFile(std::string password)
 {
-    using namespace CryptoPP;
+    std::ofstream passFile("./test/pass");
+    passFile << password;
+    passFile.close();
+}
 
-    AutoSeededRandomPool prng;
-    HexEncoder encoder(new CryptoPP::FileSink(std::cout));
+void generateKeyFile()
+{
+    CryptoPP::AutoSeededRandomPool prng;
+    CryptoPP::HexEncoder encoder(new CryptoPP::FileSink(std::cout));
 
-    SecByteBlock key(CryptoPP::AES::DEFAULT_KEYLENGTH);
-    SecByteBlock iv(CryptoPP::AES::BLOCKSIZE);
+    CryptoPP::SecByteBlock key(CryptoPP::AES::DEFAULT_KEYLENGTH);
+    CryptoPP::SecByteBlock iv(CryptoPP::AES::BLOCKSIZE);
 
     prng.GenerateBlock(key, key.size());
     prng.GenerateBlock(iv, iv.size());
 
-    std::cout << "key: ";
-    encoder.Put(key, key.size());
-    encoder.MessageEnd();
-    std::cout << std::endl;
+    // std::cout << "key: ";
+    // encoder.Put(key, key.size());
+    // encoder.MessageEnd();
+    // std::cout << std::endl;
+    //
+    // std::cout << "iv: ";
+    // encoder.Put(iv, iv.size());
+    // encoder.MessageEnd();
+    // std::cout << std::endl;
 
-    std::cout << "iv: ";
-    encoder.Put(iv, iv.size());
-    encoder.MessageEnd();
-    std::cout << std::endl;
+    std::cout << "Key: " << key.data() << std::endl;
+    // std::cout << "IV: " << iv.data() << std::endl;
+    // std::cout << "Both: " << key.data() << " " << iv.data() << std::endl;
 
-    /*********************************\
-    \*********************************/
-
-    // Convert byte to string COMPLETE!
-    std::string keyS;
-    HexEncoder encodeS;
-    encodeS.Attach(new StringSink(keyS));
-    encodeS.Put(key.data(), key.size());
-    encodeS.MessageEnd();
-    // std::cout << "Decoded string key: " << keyS << std::endl;
-
-    // Convert string hex to byte COMPLETE!
-    // std::cout << "key raw: " << key.data() << std::endl;
-    std::string destination;
-    StringSource ss(keyS, true, new HexDecoder(new StringSink(destination)));
-    const byte* result = (const byte*) destination.data();
-    // std::cout << "Decoded byte key: " << result << std::endl;
-
-    // Chec if can be written to file COMPLETE!
-    std::ofstream testFile("./test/testFile");
-    testFile << keyS << std::endl;
-    testFile << result << std::endl;
-    testFile.close();
-
-    // Check if can be read from a file COMPLETE!
-    std::string line1, line2;
-    std::ifstream readTest("./test/testFile");
-    std::getline(readTest, line1);
-    std::getline(readTest, line2);
-    // std::cout << "Line 1 from the file encoded: " << line1 << std::endl;
-    // std::cout << "Line 2 from the file raw: " << line2 << std::endl;
-    readTest.close();
-
-    // Testing encryption
-
-    std::string plain = "This is a cipher test.";
-    std::string cipher, recovered;
-
-    std::cout << "Plain text: " << plain << std::endl;
-
-    try
-    {
-        CBC_Mode< AES >::Encryption e;
-        e.SetKeyWithIV(result, 16, iv);
-
-        StringSource s(plain, true,
-            new StreamTransformationFilter(e,
-                new StringSink(cipher)
-            ) // StreamTransformationFilter
-        ); // StringSource
-    }
-    catch(const Exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-        exit(1);
-    }
-
-    // Print out hexcode of encoded plain text
-    std::cout << "cipher text: ";
-    encoder.Put((const byte*)&cipher[0], cipher.size());
-    encoder.MessageEnd();
-    std::cout << std::endl;
-
-    std::cout << "cipher text raw: " << cipher << std::endl;
-
-    // Recover the text
-    try
-    {
-        CBC_Mode< AES >::Decryption d;
-        d.SetKeyWithIV(result, 16, iv);
-
-        StringSource s(cipher, true,
-            new StreamTransformationFilter(d,
-                new StringSink(recovered)
-            ) // StreamTransformationFilter
-        ); // StringSource
-
-        std::cout << "recovered text: " << recovered << std::endl;
-    }
-    catch(const Exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-        exit(1);
-    }
+    std::ofstream keyFile("./test/key");
+    keyFile << key.data() << iv.data();
+    keyFile.close();
 }
+
+int encrypt(std::string keyFile)
+{
+    // Read key and iv from key file
+    std::ifstream readKey(keyFile, std::ios::binary);
+    if (!readKey.good())
+    {
+        std::cout << "Could not open " << keyFile << "." << std::endl;
+        return 0;
+    }
+    else {
+        // try to read in the first 50 bytes (as characters)
+        const std::size_t nbytes = 16 ;
+        std::vector<char> buff(nbytes) ; // allocate a buffer of nbytes characters
+        if( readKey.read( buff.data(), buff.size() ) ) // try to read in nbytes
+        {
+            const auto nread = readKey.gcount() ; // number of bytes that were actually read
+
+            // from the characters that were read, initialise a vector of nbytes bytes
+            std::vector<CryptoPP::byte> key( buff.begin(), buff.begin() + nread ) ;
+
+            // print out the values of the bytes (two hex digits per byte)
+            std::cout << "the first " << nbytes << " bytes in the file '"
+                      << keyFile << "' are hex:\n" << std::setfill('0') ;
+            // for( CryptoPP::byte b : bytes ) std::cout << std::hex << std::setw(2) << int(b) << ' ' ;
+            for( CryptoPP::byte b : key ) std::cout << b << ' ' ;
+            std::cout << '\n' ;
+        }
+    }
+    readKey.close();
+    return 0;
+
+    // std::string plain = "This is a cipher test.";
+    // std::string cipher, recovered;
+    //
+    // std::cout << "Plain text: " << plain << std::endl;
+    //
+    // try
+    // {
+    //     CBC_Mode< AES >::Encryption e;
+    //     e.SetKeyWithIV(result, 16, iv);
+    //
+    //     StringSource s(plain, true,
+    //         new StreamTransformationFilter(e,
+    //             new StringSink(cipher)
+    //         ) // StreamTransformationFilter
+    //     ); // StringSource
+    // }
+    // catch(const Exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    //     exit(1);
+    // }
+    //
+    // // Print out hexcode of encoded plain text
+    // std::cout << "cipher text: ";
+    // encoder.Put((const byte*)&cipher[0], cipher.size());
+    // encoder.MessageEnd();
+    // std::cout << std::endl;
+    //
+    // std::cout << "cipher text raw: " << cipher << std::endl;
+    //
+    // // Recover the text
+    // try
+    // {
+    //     CBC_Mode< AES >::Decryption d;
+    //     d.SetKeyWithIV(result, 16, iv);
+    //
+    //     StringSource s(cipher, true,
+    //         new StreamTransformationFilter(d,
+    //             new StringSink(recovered)
+    //         ) // StreamTransformationFilter
+    //     ); // StringSource
+    //
+    //     std::cout << "recovered text: " << recovered << std::endl;
+    // }
+    // catch(const Exception& e)
+    // {
+    //     std::cerr << e.what() << std::endl;
+    //     exit(1);
+    // }
+}
+
+void decrypt()
+{
+}
+
+// Function to generate a key along with iv
+// void genKey()
+// {
+//     using namespace CryptoPP;
+//
+//     AutoSeededRandomPool prng;
+//     HexEncoder encoder(new FileSink(std::cout));
+//
+//     SecByteBlock key(AES::DEFAULT_KEYLENGTH);
+//     SecByteBlock iv(AES::BLOCKSIZE);
+//
+//     prng.GenerateBlock(key, key.size());
+//     prng.GenerateBlock(iv, iv.size());
+//
+//     std::cout << "key: ";
+//     encoder.Put(key, key.size());
+//     encoder.MessageEnd();
+//     std::cout << std::endl;
+//
+//     std::cout << "iv: ";
+//     encoder.Put(iv, iv.size());
+//     encoder.MessageEnd();
+//     std::cout << std::endl;
+//
+//     /*********************************\
+//     \*********************************/
+//
+//     // Convert byte to string COMPLETE!
+//     std::string keyS;
+//     HexEncoder encodeS;
+//     encodeS.Attach(new StringSink(keyS));
+//     encodeS.Put(key.data(), key.size());
+//     encodeS.MessageEnd();
+//     // std::cout << "Decoded string key: " << keyS << std::endl;
+//
+//     // Convert string hex to byte COMPLETE!
+//     // std::cout << "key raw: " << key.data() << std::endl;
+//     std::string destination;
+//     StringSource ss(keyS, true, new HexDecoder(new StringSink(destination)));
+//     const byte* result = (const byte*) destination.data();
+//     // std::cout << "Decoded byte key: " << result << std::endl;
+//
+//     // Chec if can be written to file COMPLETE!
+//     std::ofstream testFile("./test/testFile");
+//     testFile << keyS << std::endl;
+//     testFile << result << std::endl;
+//     testFile.close();
+//
+//     // Check if can be read from a file COMPLETE!
+//     std::string line1, line2;
+//     std::ifstream readTest("./test/testFile");
+//     std::getline(readTest, line1);
+//     std::getline(readTest, line2);
+//     // std::cout << "Line 1 from the file encoded: " << line1 << std::endl;
+//     // std::cout << "Line 2 from the file raw: " << line2 << std::endl;
+//     readTest.close();
+//
+//     // Testing encryption
+//
+//     std::string plain = "This is a cipher test.";
+//     std::string cipher, recovered;
+//
+//     std::cout << "Plain text: " << plain << std::endl;
+//
+//     try
+//     {
+//         CBC_Mode< AES >::Encryption e;
+//         e.SetKeyWithIV(result, 16, iv);
+//
+//         StringSource s(plain, true,
+//             new StreamTransformationFilter(e,
+//                 new StringSink(cipher)
+//             ) // StreamTransformationFilter
+//         ); // StringSource
+//     }
+//     catch(const Exception& e)
+//     {
+//         std::cerr << e.what() << std::endl;
+//         exit(1);
+//     }
+//
+//     // Print out hexcode of encoded plain text
+//     std::cout << "cipher text: ";
+//     encoder.Put((const byte*)&cipher[0], cipher.size());
+//     encoder.MessageEnd();
+//     std::cout << std::endl;
+//
+//     std::cout << "cipher text raw: " << cipher << std::endl;
+//
+//     // Recover the text
+//     try
+//     {
+//         CBC_Mode< AES >::Decryption d;
+//         d.SetKeyWithIV(result, 16, iv);
+//
+//         StringSource s(cipher, true,
+//             new StreamTransformationFilter(d,
+//                 new StringSink(recovered)
+//             ) // StreamTransformationFilter
+//         ); // StringSource
+//
+//         std::cout << "recovered text: " << recovered << std::endl;
+//     }
+//     catch(const Exception& e)
+//     {
+//         std::cerr << e.what() << std::endl;
+//         exit(1);
+//     }
+// }
 
 // Move this to main
 void printMenu()
@@ -172,8 +299,9 @@ void printDashboard()
 
 int main (int argc, char *argv[])
 {
-    genKey();
-
+    generateKeyFile();
+    encrypt("./test/key");
+    return 0;
     std::string userStatus;
     printDashboard();
 
@@ -181,10 +309,12 @@ int main (int argc, char *argv[])
     std::cout << "If this is your first time running please type 'new'\n"
         "If not press 'Enter' to continue." << std::endl;
     std::getline(std::cin, userStatus, '\n');
+
+    // Run new user initialization
     if (userStatus == "new")
     {
+        // Get users password
         std::string newUserPassword, newUserPasswordCheck;
-
         std::cout << "Starting new user process..." << std::endl;
         while (true)
         {
@@ -197,66 +327,44 @@ int main (int argc, char *argv[])
             else break;
         }
 
+        // Generate new users key and pass files
         std::cout << "Generating necessary files..." << std::endl;
-
-        std::string newUserKeyFile = "./test/key";
-        std::string newUserPassFile = "./test/pass";
-        std::fstream keyFile, passFile;
-
-        keyFile.open(newUserKeyFile, std::ios::out);
-        if (!keyFile.is_open())
-        {
-            std::cout << "Error creating key file! Exiting!" << std::endl;
-            return -1;
-        }
-        else std::cout << "Key file generated!" << std::endl;
-        passFile.open(newUserPassFile, std::ios::out);
-        if (!passFile.is_open())
-        {
-            std::cout << "Error creating pass file! Exiting!" << std::endl;
-            return -1 ;
-        }
-        else std::cout << "Pass file generated!" << std::endl;
+        generateKeyFile();
+        std::cout << "Key file generated!" << std::endl;
+        generatePassFile(newUserPassword);
+        std::cout << "Pass file generated!" << std::endl;
         std::cout << "Necessary files created. \n"
             "Please rename and save your pass file and key file somewhere safe!\n"
             "Also remember your password for this program!" << std::endl;
-        User newUser(newUserPassword, newUserPassFile, newUserKeyFile);
+        User newUser(newUserPassword, "./test/pass", "./test/key");
         newUser.printInfo();
     }
+
+    // Get old user information
     else if (userStatus.empty())
     {
         int userChoice;
         std::string loginPass, passPath, keyPath;
 
-        std::cout << "Enter password: " << std::endl;
-        std::cin >> loginPass;
-        std::cout << "Enter password data path: " << std::endl;
-        std::cin >> passPath;
-        std::cout << "Enter your encryption key here: " << std::endl;
-        std::cin >> keyPath;
+        while (true)
+        {
+            // Get user info and validate files exist
+            std::cout << "Enter password: " << std::endl;
+            std::cin >> loginPass;
+            std::cout << "Enter password data path: " << std::endl;
+            std::cin >> passPath;
+            std::cout << "Enter your encryption key here: " << std::endl;
+            std::cin >> keyPath;
+
+            std::ifstream validatePass(passPath);
+            std::ifstream validateKey(keyPath);
+            if (!validatePass.good()) std::cout << "Could not open " << passPath << "." << std::endl;
+            else if (!validateKey.good()) std::cout << "Could not open " << keyPath << "." << std::endl;
+            else break;
+        }
 
         User oldUser(loginPass, passPath, keyPath);
         oldUser.printInfo();
-
-        // Continue validation of all entered information
-        std::cout << "Validating info..." << std::endl;
-        std::cout << std::endl;
-
-        // Check if password file exists
-        std::ifstream passFile(passPath);
-        if(!passFile.is_open())
-        {
-            std::cout << "The pass file does not exist." << std::endl;
-            return -1;
-        }
-
-        // Check if key file exists
-        std::ifstream keyFile(keyPath);
-        if(!keyFile.is_open())
-        {
-            std::cout << "The key file does not exist." << std::endl;
-            return -1;
-        }
 
         // Read password entry from password file and check if correct
         // Decrypt the pass file with the users key file
@@ -270,12 +378,8 @@ int main (int argc, char *argv[])
             std::cin >> userChoice;
             switch (userChoice)
             {
-                case 1:
-                    {
-                        std::cout << "Viewing all stored passwords\n";
-                        if(passFile.is_open()) std::cout << passFile.rdbuf();
-                        break;
-                    }
+                case 1: std::cout << "Viewing all stored passwords\n";
+                     break;
                 case 2: std::cout << "Editing existing passwords\n";
                     break;
                 case 3: std::cout << "Adding a new password\n";
