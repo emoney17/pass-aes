a985abc91d5e1ab713bb1fdf50cb135d8003d670
emoney17 <emmanuel.million@tutanota.com>
2023-01-24 18:32:39 -0600

    added tclap lib and created cmake build for proj

---
+++ CMakeFiles/safepass.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/safepass.dir/src/main.cpp.o"
+  "CMakeFiles/safepass.dir/src/main.cpp.o.d"
+  "safepass"
+  "safepass.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/safepass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
+# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1
